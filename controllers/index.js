79b4049684ee4326a87dab959af562900b67caec
Gabriel Montalvo <gmontalvoriv@users.noreply.github.com>
2016-02-14 02:26:13 -0400

    update

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,7 @@ exports.api = function (req, res) {
     res.json({ message: 'quickee-api' + ' v' + (require('../package').version)});
 };
 
-// POST /api/user/reqister
+// POST /api/reqister
 exports.register = function (req, res, next) {
   var userInfo = req.body;
 
@@ -33,23 +33,4 @@ exports.register = function (req, res, next) {
   });
 };
 
-// POST api/login
-exports.login = function (req, res, next) {
-  var username = req.body.username;
-  var password = req.body.password;
-
-  User.findOne({ username: username, upwd: password }, { password: 0, __v: 0 }, function (err, user) {
-    if (err) {
-      console.log(err);
-      return res.status(500).send();
-    }
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    req.session.user = user;
-    return res.status(200).json(user)
-  });
-};
-
+// POST /api/login
