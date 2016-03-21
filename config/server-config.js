38bcc9beaab2312df282b317c06f120a2b5b8a3c
Gabriel Montalvo <gabe.mon5@outlook.com>
2016-03-20 23:09:08 -0400

    leaving latest stable build for heroku before switching to openshift

--- config/server-config.js
+++ config/server-config.js
@@ -7,8 +7,8 @@ module.exports = {
     'secret': 'rRID4RK7'
   },
   'development': {
-    'port': Number(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3001),
-    'ip': process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    'port': Number(process.env.PORT || 3001),
+    'ip': '127.0.0.1',
     'database': 'mongodb://admin:admin@ds061355.mongolab.com:61355/quickee-db'
   },
   'test': {
