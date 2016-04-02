4fbf2abe3daf0575de9d5856fe3fbea8b80da40e
Gabriel Montalvo <gabe.mon5@outlook.com>
2016-04-01 22:37:11 -0400

    minor changes

--- config/server-config.js
+++ config/server-config.js
@@ -1,31 +1,34 @@
 'use strict';
 
 /* Environment Objects */
-var env = {
+var config = {
   production: {
     port: Number(process.env.PORT || 8080),
     host: process.env.IP || '127.0.0.1',
     database: 'mongodb://ds061355.mongolab.com:61355/quickee-db',
+    env: 'prod',
     secret: 'rRID4RK7'
   },
   development: {
     port: Number(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080),
     host: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1',
-    database: 'mongodb://ds061355.mongolab.com:61355/quickee-db' // after production this URI will point to a local or testing database
+    database: 'mongodb://ds061355.mongolab.com:61355/quickee-db', // after production this URI will point to a local or testing database
+    env: 'dev'
   },
   test: {
     port: Number(process.env.PORT || 3000),
     host: '127.0.0.1', // run tests in a local environment only
-    database: 'mongodb://ds011369.mlab.com:11369/quickee-test-db'
+    database: 'mongodb://ds011369.mlab.com:11369/quickee-test-db',
+    env: 'test'
   }
 };
 
 module.exports = function () {
   switch(process.env.NODE_ENV) {
-  case 'production': return env.production;    // production environment
-  case 'development': return env.development;  // development environment
-  case 'test': return env.test;                // test environment
-  default: return env.development; // node server will start en development mode by default
+  case 'production': return config.production;    // production environment
+  case 'development': return config.development;  // development environment
+  case 'test': return config.test;                // test environment
+  default: return config.development;             // node server will start en development mode by default
   }
 };
 
